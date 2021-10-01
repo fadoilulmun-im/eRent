dea835a24bd1c48d61f3f98a9a03d09f13d30069
il <fadoilulmun.im2807@gmail.com.com>
2021-10-01 09:55:02 +0700

    lupa password

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,5 +1,9 @@
 import argon2 from 'argon2'
+const sendEmail = require('./send-email')
+const crypto = require('crypto');
 
 export const ext = {
-  argon2
+  argon2,
+  sendEmail,
+  crypto
 }
---
+++ app/server/src/send-email.ts
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (email: any, subject: any, text: any) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: "ilulmahardika@gmail.com",
+        pass: "il Ul66@",
+      },
+    });
+
+    await transporter.sendMail({
+      from: "ERENT <ilulmahardika@gmail.com>",
+      to: email,
+      subject: subject,
+      html: text,
+    });
+
+    console.log("email sent sucessfully");
+    return true
+  } catch (error) {
+    console.log(error, "email not sent");
+    return false
+  }
+};
+
+module.exports = sendEmail;
