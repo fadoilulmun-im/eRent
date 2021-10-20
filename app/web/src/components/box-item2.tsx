ca2d297c99bc7fe8942c3c759acb04323b38a659
Ahmad Farid <ahmadfarid.gresik@gmail.com>
2021-10-20 19:45:00 +0700

    add animation notif

--- app/web/src/components/box-item2.tsx
+++ app/web/src/components/box-item2.tsx
@@ -1,8 +1,9 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
+import { motion } from "framer-motion"
 
 export default (props) => {
   const _component = useComponent("box-item2","/app/web/src/components/box-item2",{ props });
-  return eval(_component.render)
+  return (<motion.div animate={{scale:1}} initial={{scale:0.8}}>{eval(_component.render)}</motion.div>)
 }
--- app/web/src/components/notif.tsx
+++ app/web/src/components/notif.tsx
@@ -1,8 +1,22 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/react";
+import { jsx} from "@emotion/react";
 import { useComponent } from "web.utils/component";
+import React, {useState} from "react";
+import { motion } from "framer-motion"
+
 
 export default (props) => {
-  const _component = useComponent("notif","/app/web/src/components/notif",{ props });
-  return eval(_component.render)
+  //const _component = useComponent("notif","/app/web/src/components/notif",{props});
+  //const [state, setstate] = useState(0);
+  return (<motion.div animate={{y: props.state?0:-150}} initial={{y:-150}} className=" rounded-md z-10 items-center text-white bg-blue-500 flex fixed p-5 justify-between" style={{
+    top:"3rem",
+    left:"1rem",
+    right:"1rem",
+  }}>
+    <span>{props.msg}</span>
+    <span className=" border border-white rounded-md p-3">
+    <button onClick={()=>{props.click()}}>Close</button>
+    </span>
+  </motion.div>
+  );
 }
