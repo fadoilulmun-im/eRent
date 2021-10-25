ecd9357e409b4048664e03a240e1924033f4a4fe
Ahmad Farid <ahmadfarid.gresik@gmail.com>
2021-10-25 11:14:16 +0700

    add pendding order

--- app/web/src/components/save-cancel.tsx
+++ app/web/src/components/save-cancel.tsx
@@ -2,7 +2,7 @@
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
-export default ({ children }) => {
-  const _component = useComponent("save-cancel","/app/web/src/components/save-cancel",{});
+export default (props) => {
+  const _component = useComponent("save-cancel","/app/web/src/components/save-cancel",{props});
   return eval(_component.render)
 }
