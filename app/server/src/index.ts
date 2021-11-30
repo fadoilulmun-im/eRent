7f678de3e7ba5c749ec13280c3808a2747ee6cf1
Fadoilul Mun`im <fadoilulmun.im2807@gmail.com>
2021-12-01 00:08:58 +0700

    fix notif socket & insert to database

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -5,7 +5,8 @@ const fs = require('fs');
 const path = require("path");
 const pump = require("pump");
 const nowLocal = require("./function")
-const  {main} = require('../../db/src/main/index');
+// const  {main} = require('../../db/src/main/index');
+import { api } from 'web.utils/src/api';
 
 // socket untuk client di api (ilul)
 import { io } from "socket.io-client";
@@ -24,14 +25,21 @@ ioServer.on("connection", (socket) => {
       let tim = new Date(e.time).getTime()  - new Date().getTime();
 
       setTimeout(async() => {
-        const notif = await main.notif.create({
-          data:{
-            title: e.data.title,
-            desc: e.data.desc,
-            created_at: new Date(nowLocal()),
-            id_customer: e.user_id
-          }
+        // const notif = await main.notif.create({
+        //   data:{
+        //     title: e.data.title,
+        //     desc: e.data.desc,
+        //     created_at: new Date(nowLocal()),
+        //     id_customer: e.user_id
+        //   }
+        // });
+        // e.data.created_at = notif.created_at;
+        const notif = await api("/api/notif/create", {
+          id_customer: e.user_id,
+          title: e.data.message,
+          desc: e.data.desc
         });
+
         e.data.created_at = notif.created_at;
 
         ioServer.emit(e.event + "_" + e.user_id, e.data);
